8720fce0c8823a795386ee4d13394e34f62130ec
Corentin Chary <corentin.chary@gmail.com>
2023-08-17 06:58:17 +0000

    Move files

--- screen/update.py
+++ screen/update.py
@@ -1,28 +1,47 @@
+import sys
 import logging
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+import requests
+import argparse
 
 from PIL import Image
 
 import epd7in5_V2
 
-def init_screen():
-    print("init")
+def setup_logging():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image")
+    parser.add_argument("--dry-run", action="store_true")
+    return parser.parse_args(args)
+
+def init_screen(dry_run=False):
+    logging.info("Init screen")
+    if dry_run:
+        return None
     epd = epd7in5_V2.EPD()
     epd.init()
     epd.Clear()
     return epd
 
-def write_to_screen(epd):
-    print("write")
-    h_image = Image.new('1', (epd.width, epd.height), 255)
-    bmp = Image.open("screen.bmp")
+def write_to_screen(epd, image):
+    logging.info("Writing to screen...")
+    width, height = (800, 480)
+    if epd:
+        width, height = epd.width, epd.height
+
+    h_image = Image.new('1', (width, height), 255)
+    bmp = Image.open(image)
+
+    # TODO: Support resizes
     h_image.paste(bmp, (0, 0))
 
-    epd.init()
-    epd.display(epd.getbuffer(h_image))
-    epd.sleep()
+    if epd:
+        epd.init()
+        epd.display(epd.getbuffer(h_image))
+        epd.sleep()
 
 
 def close():
@@ -31,8 +50,13 @@ def close():
     exit()
 
 def main():
-    edp = init_screen()
-    write_to_screen(edp)
+    args = parse_args(sys.argv[1:])
+    setup_logging()
+    # TODO:
+    # -download image (if necessary)
+    image = args.image
+    edp = init_screen(args.dry_run)
+    write_to_screen(edp, image)
     
 if __name__ == '__main__':
     try:
