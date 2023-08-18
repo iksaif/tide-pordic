40e5100647434f98125dec751456be5534ac5338
Corentin Chary <corentin.chary@gmail.com>
2023-08-18 10:13:26 +0000

    add missing files

---
+++ screen/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+
+URL="$1"
+SCREEN="screen.bmp"
+
+TEMP=$(mktemp "$(basename "$0")-XXX")
+trap "rm -f ${TEMP}" EXIT
+
+curl ${URL} -o ${TEMP}
+convert ${TEMP} -dither FloydSteinberg -define dither:diffusion-amount=90%  -remap eink-2color.png BMP3:${SCREEN}
+python3 update.py ${SCREEN}
